16199c5fd1c3bb4e832fd4454a9461fefccc9c5f
k-ing-ing <1762507696@qq.com>
2025-10-16 12:41:35 +0800

    完成“不同字符统计程序”作业

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-letters = 0    # 英文字符计数
-digits = 0     # 数字计数
-spaces = 0     # 空格计数
-others = 0     # 其他字符计数
-
-# 从键盘输入一行字符
-line = input()
-
-# 遍历输入的每个字符并统计
-for char in line:
-    if char.isalpha():
-        letters += 1
-    elif char.isdigit():
-        digits += 1
-    elif char.isspace():
-        spaces += 1
-    else:
-        others += 1
-
-# 按照要求的格式输出结果
-print(f"英文字符: {letters}")
-print(f"数字: {digits}")
-print(f"空格: {spaces}")
-print(f"其他字符: {others}")
+def char_count(s):
+    eng = num = space = other = 0
+    for c in s:
+        if c.isalpha() and c.isascii():  # 只统计英文字母
+            eng += 1
+        elif c.isdigit():
+            num += 1
+        elif c == ' ':
+            space += 1
+        else:
+            other += 1
+    print(f'英文字符：{eng}')
+    print(f'数字：{num}')
+    print(f'空格：{space}')
+    print(f'其他字符：{other}')
