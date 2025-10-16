dc2d12e0ee97d5725d16290090f1bda2a799b48b
k-ing-ing <1762507696@qq.com>
2025-10-16 12:52:07 +0800

    完成”不同字符统计“作业

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def char_count(s):
             other += 1
     # 一次性输出，避免 print 多次导致多余空行
     print(f'英文字符：{eng}\n数字：{num}\n空格：{space}\n其他字符：{other}')
-
 if __name__ == "__main__":
     s = input()
     char_count(s)
