2258d989521a3340d118d25254ed3192f0da23bf
k-ing-ing <1762507696@qq.com>
2025-10-16 13:08:19 +0800

    完成”不同字符统计“作业

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-def char_count(s):
-    eng = num = space = other = 0
-    for c in s:
-        if c.isalpha() and c.isascii():
-            eng += 1
-        elif c.isdigit():
-            num += 1
-        elif c == ' ':
-            space += 1
-        else:
-            other += 1
-    return "英文字符：{0}\n数字：{1}\n空格：{2}\n其他字符：{3}".format(eng, num, space, other)
+s = input()  # 读取一行输入
+
+letters = 0
+digits = 0
+spaces = 0
+others = 0
+
+for char in s:
+    if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+        letters += 1
+    elif '0' <= char <= '9':
+        digits += 1
+    elif char == ' ':  # 只统计空格字符
+        spaces += 1
+    else:
+        others += 1
+
+print(f"英文字符: {letters}")
+print(f"数字: {digits}")
+print(f"空格: {spaces}")
+print(f"其他字符: {others}")
